cb2419f6da1ea7044c4a9b983a946300e12b35d7
rproct <rproctor@csumb.edu>
2019-05-03 00:33:08 -0700

    Cleaned up Map() class but needs work, still needs textbox and button attachment

--- specAssist.py
+++ specAssist.py
@@ -1,6 +1,6 @@
 import sys
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.garden.mapview import MapView, MapMarker
+from kivy.garden.mapview import MapView, MapMarker, MapSource
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.lang import Builder
@@ -9,32 +9,27 @@ from kivy.base import runTouchApp
 #Required for the search screen to work
 Builder.load_file('layout.kv')
 
-if __name__ == '__main__' and __package__ is None:
-    from os import sys, path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
 #Search screen, currently no triggers or slots yet
 class Search(AnchorLayout):
     pass
 
-class Map():
-    #https://mapview.readthedocs.io/en/latest/#mapview.MapSource
-    kwargs = {}
-    #Supposed to go in the url
-    x = 36.654131
-    y = -121.798172
-    z = 1
-    if len(sys.argv) > 1:
-        #Open source map, currently defaults to entire earth zoomed out
-        kwargs["map_source"] = MapSource(url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", attribution="")
-    #Opens Map, not yet interactive
-    runTouchApp(MapView(**kwargs))
+#Commented out because maps opens by default not opened when called
+"""class Map():
+    #https://mapview.readthedocs.io/en/latest/
+    #Lat and Lon need to be in the degree decimal format
+    latitude = 36.604340
+    longitude = -121.854599
+    #Min zoom: 0, Max zoom: 20
+    magnify = 15
+    #Map is provided by default, just needed to pass these values in
+    map = MapView(zoom=magnify, lon=longitude, lat=latitude)
+    #Open Map
+    runTouchApp(map)"""
 
 class Driver(App):
     title = 'Spec Assist'
     def build(self):
-        #Currently set to Map() for debugging
-        return Map()
+        return Search()
 
 #Starts the program
 if __name__ == '__main__':
